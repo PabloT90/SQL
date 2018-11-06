566ef0da200f1d2a3466ef222116a77d353234cf
PabloT90 <36712068+PabloT90@users.noreply.github.com>
2018-11-06 14:37:17 +0100

    v1.0
    Ejercicio HuertosEcologicos terminado.

--- Primer Trimestre/HuertoEcologicos.sql
+++ Primer Trimestre/HuertoEcologicos.sql
@@ -4,7 +4,7 @@ USE HuertosEcologicos
 GO
 
 CREATE TABLE agricultores(
-id char(8) CONSTRAINT pk_agricultores PRIMARY KEY,
+id tinyint IDENTITY(1,1) CONSTRAINT pk_agricultores PRIMARY KEY,
 nombre varchar(10) null,
 direccion varchar(15) null,
 telefono char(9) not null,
@@ -14,9 +14,10 @@ CREATE TABLE tractores(
 matricula char(7) CONSTRAINT pk_tractores PRIMARY KEY,
 marca varchar(10) null,
 modelo varchar(15) null,
-id_agricultor char(8) not null,
+id_agricultor tinyint not null,
 
 CONSTRAINT fk_tractor_agricultor FOREIGN KEY (id_agricultor) REFERENCES agricultores(id)
+	ON DELETE NO ACTION ON UPDATE CASCADE
 )
 
 CREATE TABLE fincas(
@@ -26,19 +27,21 @@ municipio varchar(10) not null,
 )
 
 CREATE TABLE lotes(
-id char(6) CONSTRAINT pk_lotes PRIMARY KEY,
+id tinyint IDENTITY(1,1) CONSTRAINT pk_lotes PRIMARY KEY,
 peso tinyint null,
 fecha datetime not null,
 finca_procedencia char(10) not null,
 variedad varchar(20) not null,
-id_agricultor char(8) not null,
+id_agricultor tinyint not null,
 
-CONSTRAINT fk_lotes_finca FOREIGN KEY (finca_procedencia) REFERENCES fincas(nombre),
+CONSTRAINT fk_lotes_finca FOREIGN KEY (finca_procedencia) REFERENCES fincas(nombre)
+	ON DELETE NO ACTION ON UPDATE CASCADE,
 CONSTRAINT fk_lotes_agricultor FOREIGN KEY (id_agricultor) REFERENCES agricultores(id)
+	ON DELETE NO ACTION ON UPDATE CASCADE
 )
 
 CREATE TABLE distribuidoras(
-id char(3) CONSTRAINT pk_distribuidoras PRIMARY KEY,
+id tinyint not null IDENTITY(1,1) CONSTRAINT pk_distribuidoras PRIMARY KEY,
 nombre varchar(7) not null,
 direccion varchar(15) not null,
 telefono char(9) not null,
@@ -48,9 +51,16 @@ CREATE TABLE remesas(
 id char(5) CONSTRAINT pk_remesas PRIMARY KEY,
 acidez char(4) not null,
 cantidad tinyint null,
-id_lote char(6) not null,
-id_distribuidora char(3) not null,
+id_lote tinyint not null,
+id_distribuidora tinyint null,
 
-CONSTRAINT fk_remesa_lote FOREIGN KEY (id_lote) REFERENCES lotes(id),
-CONSTRAINT fk_remesa_distribuidora FOREIGN KEY (id_distribuidora) REFERENCES distribuidoras(id),
+CONSTRAINT uq_remesa_lote UNIQUE (id_lote),
+CONSTRAINT fk_remesa_lote FOREIGN KEY (id_lote) REFERENCES lotes(id)
+	ON DELETE NO ACTION ON UPDATE CASCADE,
+CONSTRAINT fk_remesa_distribuidora FOREIGN KEY (id_distribuidora) REFERENCES distribuidoras(id)
+	ON DELETE NO ACTION ON UPDATE CASCADE
 )
+
+--Cuando tenemos una 1:1 y las cardinalidades son (1,1) desde donde propagamos la clave, su clave foranea debe ser NOT NULL.
+--Recordar poner las restriccion UNIQUE cuando se trata de 1:1
+--Preguntar cuando cambiar ON DELETE NO ACTION ON UPDATE CASCADE
