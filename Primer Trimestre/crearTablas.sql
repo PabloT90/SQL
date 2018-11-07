8a718b5bc1c1d83c88974e8f0b1087c36aa7128b
Pablo <36712068+PabloT90@users.noreply.github.com>
2018-11-07 14:17:52 +0100

    v1.0
    Corregidos errores y añadido un pequeño recordatorio para saber cuando usar qué cosas.

--- Primer Trimestre/crearTablas.sql
+++ Primer Trimestre/crearTablas.sql
@@ -4,23 +4,23 @@ USE Universidades
 GO
 
 CREATE TABLE universidades(
-id char(8) CONSTRAINT PK_universidades PRIMARY KEY,
+id tinyint not null CONSTRAINT PK_universidades PRIMARY KEY,
 nombre varchar(15) null,
 fundacion datetime null,
 )
 
 CREATE TABLE modalidades(
-id char(2) CONSTRAINT PK_modalidades PRIMARY KEY,
+id tinyint not null CONSTRAINT PK_modalidades PRIMARY KEY,
 isIndividual bit not null,
 )
 
 CREATE TABLE deportes(
-id char(3) CONSTRAINT PK_deportes PRIMARY KEY,
+id tinyint not null CONSTRAINT PK_deportes PRIMARY KEY,
 nombre varchar(10) null,
 )
 
 CREATE TABLE deportistas(
-id char(6) CONSTRAINT PK_deportistas PRIMARY KEY,
+id tinyint not null CONSTRAINT PK_deportistas PRIMARY KEY,
 id_universidad char(8) not null,
 id_deportes char(3) not null,
 nombre varchar(10) null,
@@ -34,48 +34,56 @@ CONSTRAINT FK_deportistas_deportes FOREIGN KEY (id_deportes) REFERENCES deportes
 )
 
 CREATE TABLE deportistas_modalidades (
-id_deportistas char(6)not null,
-id_modalidades char(2) not null,
+id_deportistas tinyint not null,
+id_modalidades tinyint not null,
 clasificacion tinyint not null,
 
 CONSTRAINT PK_deportistas_modalidades PRIMARY KEY(id_deportistas, id_modalidades),
-CONSTRAINT FK_deportistas_modalidades FOREIGN KEY (id_deportistas) REFERENCES deportistas (id),
-CONSTRAINT FK_deportistas_modalidades2 FOREIGN KEY (id_modalidades) REFERENCES modalidades (id),
+CONSTRAINT FK_deportistas_modalidades FOREIGN KEY (id_deportistas) REFERENCES deportistas (id)
+	ON DELETE NO ACTION ON UPDATE CASCADE,
+CONSTRAINT FK_deportistas_modalidades2 FOREIGN KEY (id_modalidades) REFERENCES modalidades (id)
+	ON DELETE NO ACTION ON UPDATE CASCADE,
 )
 
 CREATE TABLE comites(
-id char(7) CONSTRAINT PK_comites PRIMARY KEY,
-id_universidad char(8) not null,
+id tinyint not null CONSTRAINT PK_comites PRIMARY KEY,
+id_universidad tinyint not null,
 domicilio varchar(20) null,
 telefono char(9) null,
 
 CONSTRAINT FK_comites_universidades FOREIGN KEY (id_universidad) REFERENCES universidades(id)
-	ON DELETE NO ACTION ON UPDATE CASCADE
+	ON DELETE CASCADE ON UPDATE CASCADE
 )
 
 CREATE TABLE presidentes(
-id char(6) CONSTRAINT PK_presidente PRIMARY KEY,
+id tinyint not null CONSTRAINT PK_presidente PRIMARY KEY,
 nombre varchar(10) null,
 apellidos varchar(15) null,
 )
 
 CREATE TABLE presidente_comite(
-id_comite char(7) not null,
-id_presidente char(6) not null,
+id_comite tinyint not null,
+id_presidente tinyint not null,
 
 CONSTRAINT PK_presidente_comite PRIMARY KEY (id_comite, id_presidente),
-CONSTRAINT FK_presidente_comite FOREIGN KEY (id_comite) REFERENCES comites(id),
+CONSTRAINT FK_presidente_comite FOREIGN KEY (id_comite) REFERENCES comites(id)
+	ON DELETE NO ACTION ON UPDATE CASCADE,
 CONSTRAINT FK_presidente_comite3 FOREIGN KEY (id_presidente) REFERENCES presidentes(id)
+	ON DELETE NO ACTION ON UPDATE CASCADE
 )
 
 CREATE TABLE delegados(
-id char(6) CONSTRAINT PK_delegados PRIMARY KEY,
-id_comite char(7) not null,
-id_deporte char(3) not null,
+id tinyint not null CONSTRAINT PK_delegados PRIMARY KEY,
+id_comite tinyint not null,
+id_deporte tinyint not null,
 nombre varchar(10) null,
 apellidos varchar(15) null,
 
 CONSTRAINT FK_delegados_comite FOREIGN KEY (id_comite) REFERENCES comites(id)
 	ON DELETE NO ACTION ON UPDATE CASCADE,
 CONSTRAINT FK_delegados_deportes FOREIGN KEY (id_deporte) REFERENCES deportes(id)
+	ON DELETE NO ACTION ON UPDATE CASCADE
 )
+
+--Si es reflexiva es no action las 2.
+--Si es debil las 2 son cascade.
