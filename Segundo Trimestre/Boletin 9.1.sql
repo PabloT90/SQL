301032cecff699f265a4f2f54e648a27c4a8d72a
PabloT90 <36712068+PabloT90@users.noreply.github.com>
2019-01-23 19:24:49 +0100

    Boleetin 9.1
    Casi terminado, faltan ejericicios con subconsultas que aun no lo hemos dado.

--- Segundo Trimestre/Boletin 9.1.sql
+++ Segundo Trimestre/Boletin 9.1.sql
@@ -42,22 +42,44 @@ SELECT DISTINCT E.EmployeeID, E.LastName, E.FirstName, E.HomePhone FROM Employee
 		WHERE CompanyName IN('Bon app''','Meter Franken')
 
 --Empleados (ID, nombre, apellidos, mes y día de su cumpleaños) que no han vendido nunca nada a ningún cliente de Francia. *
-
+--Se necesitan subconsultas (aun no dadas)
 
 --Total de ventas en US$ de productos de cada categoría (nombre de la categoría).
-
+SELECT * FROM Categories
+SELECT * FROM Products
+SELECT * FROM [Order Details]
+SELECT C.CategoryName, SUM(OD.Quantity*OD.UnitPrice) as[Total $] FROM [Order Details] AS[OD]
+	INNER JOIN Products AS[P] ON OD.ProductID = P.ProductID
+	INNER JOIN Categories AS[C] ON C.CategoryID = P.CategoryID
+		GROUP BY C.CategoryName
 
 --Total de ventas en US$ de cada empleado cada año (nombre, apellidos, dirección).
-
+SELECT * FROM Employees
+SELECT * FROM Orders
+SELECT * FROM [Order Details]
+SELECT SUM(UnitPrice*Quantity) AS[Total $], E.FirstName, E.LastName, E.Address, YEAR(O.OrderDate) AS[Año]  FROM [Order Details] AS[OD]
+	INNER JOIN Orders AS[O] ON OD.OrderID = O.OrderID
+	INNER JOIN Employees AS[E] ON O.EmployeeID = E.EmployeeID
+		GROUP BY E.EmployeeID, E.FirstName, E.LastName, E.Address, YEAR(O.OrderDate)
+			ORDER BY YEAR(O.OrderDate)
 
 --Ventas de cada producto en el año 97. Nombre del producto y unidades.
-
+SELECT * FROM Products
+SELECT * FROM [Order Details]
+SELECT * FROM Orders
+SELECT SUM(OD.Quantity) AS[Total Producto], P.ProductName FROM Orders AS[O]
+	INNER JOIN [Order Details] as[OD] ON O.OrderID = OD.OrderID
+	INNER JOIN Products AS[P] ON OD.ProductID = P.ProductID
+		WHERE YEAR(O.OrderDate) = 1997
+		GROUP BY P.ProductName
 
 --Cuál es el producto del que hemos vendido más unidades en cada país. *
-
+--Se necesitan subconsultas (aun no dadas)
 
 --Empleados (nombre y apellidos) que trabajan a las órdenes de Andrew Fuller.
-
+SELECT * FROM Employees
+SELECT LastName, FirstName FROM Employees
+	WHERE ReportsTo = 2 --Esta forma no me gusta pero es las mas facil en esta situacion
 
 --Número de subordinados que tiene cada empleado, incluyendo los que no tienen ninguno. Nombre, apellidos, ID.
 
