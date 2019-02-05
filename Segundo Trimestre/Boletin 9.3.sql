cb1c0095a3b075c409d1fd4a0869256626774db7
PabloT90 <36712068+PabloT90@users.noreply.github.com>
2019-02-05 14:30:23 +0100

    Update Boletin 9.3.sql

--- Segundo Trimestre/Boletin 9.3.sql
+++ Segundo Trimestre/Boletin 9.3.sql
@@ -1,5 +1,18 @@
 --1. Título y tipo de todos los libros en los que alguno de los autores vive en California (CA).
+SELECT * FROM titles
+SELECT * FROM titleauthor
+SELECT * FROM authors
+SELECT T.type, T.title FROM titles AS[T]
+	INNER JOIN titleauthor AS[TA] ON T.title_id = TA.title_id
+	INNER JOIN authors AS[A] ON TA.au_id = A.au_id
+	WHERE A.State = 'CA'
+
 --2. Título y tipo de todos los libros en los que ninguno de los autores vive en California (CA).
+SELECT T.type, T.title FROM titles AS[T]
+	INNER JOIN titleauthor AS[TA] ON T.title_id = TA.title_id
+	INNER JOIN authors AS[A] ON TA.au_id = A.au_id
+	WHERE A.state != 'CA'
+
 --3. Número de libros en los que ha participado cada autor, incluidos los que no han publicado nada.
 --4. Número de libros que ha publicado cada editorial, incluidas las que no han publicado ninguno.
 --5. Número de empleados de cada editorial.
