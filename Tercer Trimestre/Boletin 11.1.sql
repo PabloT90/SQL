b1c3513561737bb7cd467dbad4a731dc14c0e81d
PabloT90 <36712068+PabloT90@users.noreply.github.com>
2019-03-26 17:25:52 +0100

    Update Boletin 11.1.sql

--- Tercer Trimestre/Boletin 11.1.sql
+++ Tercer Trimestre/Boletin 11.1.sql
@@ -13,17 +13,62 @@ IF NOT EXISTS(SELECT * FROM Products
 		VALUES('Cruzcampo Lata', 16, 1, 'Pack 6 Latas', 4.40, NULL, NULL, NULL, 0)
 ELSE
 	UPDATE Products
-		SET UnitPrice = 4.40
+		SET UnitPrice = 4.30
 	WHERE ProductName = 'Cruzcampo Lata'
 END
-ROLLBACK
+--COMMIT TRAN
+--ROLLBACK
+
 --2) Comprueba si existe una tabla llamada ProductSales. Esta tabla ha de tener de cada producto el ID, el Nombre, el Precio unitario, 
 --el número total de unidades vendidas y el total de dinero facturado con ese producto. Si no existe, créala
+IF EXISTS (SELECT * FROM sysobjects WHERE name = 'ProductSales')
+	BEGIN
+		PRINT 'Existe'
+	END
+ELSE
+	BEGIN
+		BEGIN TRAN
+		CREATE TABLE [ProductSales](
+			ID int NOT NULL,
+			Nombre varchar(40) not null,
+			UnitPrice money null,
+			SoldUnits smallint null,
+			DineroFacturado money null,
+		)
 
+		--Copio los datos de la tabla products a esta
+		BEGIN TRAN
+		INSERT INTO ProductSales
+		SELECT ProductID, ProductName, UnitPrice, UnitsOnOrder, UnitPrice*UnitsInStock FROM Products
+	END --BEGIN
+
+--COMMIT TRAN
+--ROLLBACK
+
+--Otra forma, segun he leido es mejor que la anterior.
+IF OBJECT_ID('dbo.ProductSales') IS NULL
+	BEGIN
+		BEGIN TRAN
+		CREATE TABLE [ProductSales](
+			ID int NOT NULL,
+			Nombre varchar(15) not null,
+			UnitPrice money null,
+			SoldUnits int null,
+			DineroFacturado money null,
+		)
+	END --BEGIN
+ELSE
+	BEGIN
+		PRINT 'Existe'
+	END --BEGIN
+--COMMIT TRAN
+--ROLLBACK
 
 --3) Comprueba si existe una tabla llamada ShipShip. Esta tabla ha de tener de cada Transportista el ID, el Nombre de la compañía, 
 --el número total de envíos que ha efectuado y el número de países diferentes a los que ha llevado cosas. Si no existe, créala
 
+--Hasta estar seguro de que tanto el 3 como el 4 se resuelven como el 2, los dejare sin hacer.
+--Creo que tienen el mismo mecanismo que el 2.
 
 --4) Comprueba si existe una tabla llamada EmployeeSales. Esta tabla ha de tener de cada empleado su ID, el Nombre completo, el 
 --número de ventas totales que ha realizado, el número de clientes diferentes a los que ha vendido y el total de dinero facturado. 
