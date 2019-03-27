40c1ee07987353e4406f081db8b12682741f0548
Pablo <36712068+PabloT90@users.noreply.github.com>
2019-03-27 14:06:42 +0100

    Update Boletin 11.1.sql
    Terminado

--- Tercer Trimestre/Boletin 11.1.sql
+++ Tercer Trimestre/Boletin 11.1.sql
@@ -67,8 +67,12 @@ ELSE
 --3) Comprueba si existe una tabla llamada ShipShip. Esta tabla ha de tener de cada Transportista el ID, el Nombre de la compañía, 
 --el número total de envíos que ha efectuado y el número de países diferentes a los que ha llevado cosas. Si no existe, créala
 
+--Igual que el 2.
+
 --Hasta estar seguro de que tanto el 3 como el 4 se resuelven como el 2, los dejare sin hacer.
 --Creo que tienen el mismo mecanismo que el 2.
+
+--Igual que el 2.
 
 --4) Comprueba si existe una tabla llamada EmployeeSales. Esta tabla ha de tener de cada empleado su ID, el Nombre completo, el 
 --número de ventas totales que ha realizado, el número de clientes diferentes a los que ha vendido y el total de dinero facturado. 
@@ -78,3 +82,33 @@ ELSE
 --5) Entre los años 96 y 97 hay productos que han aumentado sus ventas y otros que las han disminuido. Queremos cambiar el precio unitario 
 --según la siguiente tabla:
 
+--incremento de ventas	Incremento del precio
+--negativo				-10%
+--entre 0% y 10%		No varia
+--entre 10% y 50%		+5%
+--mayor del 50%			10% con un maximo de 2,25
+
+SELECT * FROM VentasDel96
+SELECT * FROM VentasDel97
+
+--Realmente esta vista no haria falta pero weno.
+GO
+CREATE VIEW Diferencia AS(
+SELECT V97.ProductName, (V96.Ventas96*100)/V97.Ventas97 AS[Porcentaje] FROM VentasDel96 AS[V96]
+	INNER JOIN VentasDel97 AS[V97] ON V96.ProductName = V97.ProductName
+)
+GO
+SELECT * FROM Products
+BEGIN TRAN
+UPDATE Products
+	SET UnitPrice = (
+		CASE  
+			WHEN D.Porcentaje < 0 THEN UnitPrice * 0.9
+			WHEN D.Porcentaje BETWEEN 0 AND 10 THEN UnitPrice
+			WHEN D.Porcentaje BETWEEN 11 AND 50 THEN UnitPrice * 1.05
+			WHEN UnitPrice * 1.1 > 2.25 THEN UnitPrice + 2.25
+			WHEN UnitPrice <= 2.25 THEN UnitPrice * 1.1
+		 END   
+	)
+FROM Diferencia AS[D] --Aqui se podria hacer un inner join con las ventas del 96 y 97 y me ahorro hacer una vista extra.
+--rollback
