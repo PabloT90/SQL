5e85c96f8041106ef1839421c12790754a899012
Pablo <36712068+PabloT90@users.noreply.github.com>
2019-04-01 13:33:28 +0200

    Update Boletin 11.2.sql
    Terminado.

--- Tercer Trimestre/Boletin 11.2.sql
+++ Tercer Trimestre/Boletin 11.2.sql
@@ -1,16 +1,12 @@
 USE pubs
-
 --Tienes que ir recorriendo la tabla con un bucle, leer cada fila y realizar la actualización que se indique.
 --Si la columna TipoActualiza contiene una "I" hay que insertar una nueva fila en titles con todos los datos leídos de esa 
 --fila de ActualizaTitles.
 --Si TipoActualiza contiene una "D" hay que eliminar la fila cuyo código (title_id) se incluye.
 --Si TipoActualiza es "U" hay que actualizar la fila identificada por title_id con las columnas que no sean Null. 
 --Las que sean Null se dejan igual.
-SELECT * FROM ActualizaTitles
-SELECT * FROM titles
-SELECT * FROM publishers
---Lo que hay que hacer es un bucle que segun TipoActualiza haga una cosa u otra.
 
+--Lo que hay que hacer es un bucle que segun TipoActualiza haga una cosa u otra.
 BEGIN TRAN
 DECLARE @cont TINYINT
 SET @cont = 1
@@ -48,10 +44,21 @@ BEGIN
 	IF (SELECT tipoActualiza FROM ActualizaTitles
 			WHERE NumActualiza = @cont) = 'U'
 			--Actualizo lo que me pide
-
-
-	PRINT @COnt
-
+			UPDATE titles
+				SET
+					title_id = isnull(AT.title_id,T.title_id),
+					title = isnull(AT.title,T.title),
+					type =  isnull(AT.type,T.type),
+					pub_id = isnull(AT.pub_id,T.pub_id),
+					price = isnull(AT.price,T.price),
+					advance = isnull(AT.advance,T.advance),
+					royalty = isnull(AT.royalty,T.royalty),
+					ytd_sales = isnull(AT.ytd_sales,T.ytd_sales),
+					notes = isnull(AT.notes,T.notes),
+					pubdate = isnull(AT.pubdate,T.pubdate)
+				FROM ActualizaTitles AS AT
+					INNER JOIN titles AS[T] ON T.title_id = AT.title_id
+				WHERE NumActualiza = @cont
 	--Actualizar el contador a 1 numero mas.
 	SET @cont = @cont +1
 END --fin del while
