afb63e3460378bf999dab24ed507b42d57b57aee
Pablo <pabloprats18@gmail.com>
2019-05-16 22:25:36 +0200

    Update Boletin 12.3.sql

--- Tercer Trimestre/Boletin 12.3.sql
+++ Tercer Trimestre/Boletin 12.3.sql
@@ -29,7 +29,50 @@ ROLLBACK
 
 
 --2.- Haz un trigger para que un cliente no pueda hacer más de 10 pedidos al año (años naturales) de la misma categoría
+GO
+CREATE TRIGGER DiezXAnho ON [Order Details] AFTER INSERT AS
+BEGIN
+	DECLARE @anio INT
+	DECLARE @cliente NCHAR(5)
 
+	--Cogemos el año, el CustomerID de la tabla inserted
+	SELECT @cliente = CustomerID, @anio = YEAR(OrderDate) FROM inserted AS[I]
+		INNER JOIN Orders AS[O] ON I.OrderID = O.OrderID
+	PRINT @cliente
+	PRINT @anio
+
+	--Miramos que no tenga mas de 10 pedidos con productos 
+	IF EXISTS(SELECT CategoryID FROM Orders AS [O]
+				  INNER JOIN [Order Details] AS[OD] ON O.OrderID = OD.OrderID
+				  INNER JOIN Products AS[P] ON OD.ProductID = P.ProductID
+			  WHERE CustomerID = @cliente AND YEAR(OrderDate) = @anio
+			  GROUP BY CategoryID
+			  HAVING COUNT(CategoryID) > 10)
+	ROLLBACK
+END
+GO
+SET DATEFORMAT YMD
+--Pruebas
+SELECT * FROM [Order Details]
+SELECT * FROM Products
+	WHERE CategoryID = 1
+--Pruebas
+BEGIN TRAN
+SET IDENTITY_INSERT ORDERS ON --Para un error que da por el INSERT-IDENTITY OFF
+INSERT INTO Orders (OrderID, CustomerID, OrderDate)VALUES(12001, 'VINET', DATETIMEFROMPARTS(2019,05,16,16,00,00,00))
+--BEGIN TRAN
+INSERT INTO [Order Details] VALUES
+(12001,1,1,1,0),(12001,2,1,1,0),(12001,24,1,1,0),(12001,34,4,1,0),(12001,35,1,1,0),(12001,38,1,1,0),
+(12001,39,1,1,0),(12001,43,1,1,0),(12001,67,1,1,0),(12001,70,1,1,0),(12001,75,1,1,0)
+ROLLBACK
+
+--No haya pedido productos de la misma categoria mas de 10 veces ese año.
+SELECT COUNT(CategoryID) FROM Orders AS [O]
+	INNER JOIN [Order Details] AS[OD] ON O.OrderID = OD.OrderID
+	INNER JOIN Products AS[P] ON OD.ProductID = P.ProductID
+	WHERE CustomerID = 'VINET' AND YEAR(OrderDate) = 2019
+	GROUP BY CategoryID
+	HAVING COUNT(CategoryID) > 10
 
 --3.- Haz un trigger que no permita que un empleado sea superior de otro (ReportsTo) si el segundo es su superior 
 --(en uno o varios niveles).
@@ -41,6 +84,7 @@ ROLLBACK
 --5.- Haz un trigger que impida que pueda haber a la venta más de 30 productos de una misma categoría. Los productos que 
 --están a la venta son los que tienen un "0” en la columna "discontinued”
 SELECT * FROM Products
+	order by CategoryID
 GO
 CREATE TRIGGER MismaCategoria ON Products AFTER UPDATE,INSERT AS
 BEGIN
